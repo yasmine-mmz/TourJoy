c0ce217275bcd5d74810890fb1a00832247e35ed
iheb09 <iheb.souissi@esprit.tn>
2024-02-15 21:26:22 +0100

    Added possiblity to choose subscription from front office
    undone:
    control de saisie

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,29 +6,64 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SubscriptionRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Subscription;
+use Doctrine\Persistence\ManagerRegistry;
 
 class TestController extends AbstractController
 {
+    private ManagerRegistry $registry;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
         return $this->render('FrontOffice/front_template.html.twig', [
             'controller_name' => 'TestController',
         ]);
-        
     }
+    
     #[Route('/back', name: 'app_back')]
     public function back(): Response
     {
         return $this->render('BackOffice/test.html.twig', [
             'controller_name' => 'BackController',
         ]);
-        
     }
+    
     #[Route('/showSF', name: 'showSF')]
     public function showF(SubscriptionRepository $rep): Response
     {
         $subscriptionList = $rep->findAll();
         return $this->render('subscription/subscriptionsF.html.twig', ['subscriptionList'=>$subscriptionList]);
     }
+    
+    #[Route('/mySubscriptions', name: 'mySubscriptions')]
+    public function mySubs(SubscriptionRepository $rep): Response
+    {
+        $subscriptionList = $rep->findAll();
+        return $this->render('subscription/mySubscriptions.html.twig', ['subscriptionList' => $subscriptionList]);
+    }
+
+    #[Route('/choose-subscription', name: 'choose_subscription')]
+    public function chooseSubscription(Request $request, SubscriptionRepository $rep): Response
+    {
+        $subscriptionList = $rep->findAll();
+        $selectedSubscriptionId = $request->request->get('selected_subscription');
+
+        
+        $entityManager = $this->registry->getManager();
+        $selectedSubscription = $entityManager->getRepository(Subscription::class)->find($selectedSubscriptionId);
+
+        
+        return $this->render('subscription/chosensub.html.twig', [
+            'selectedSubscription' => $selectedSubscription,
+            
+        ]);
+    }
+
 }
